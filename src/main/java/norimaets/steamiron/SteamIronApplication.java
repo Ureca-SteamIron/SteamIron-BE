5ec134f4611acc7035bb1fe1268facf4b8b89fee
jerry7133 <jerrydevengineer@gmail.com>
2026-07-14 10:25:24 +0900

    feat: init spring

---
+++ src/main/java/norimaets/steamiron/SteamIronApplication.java
@@ -0,0 +1,13 @@
+package norimaets.steamiron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamIronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamIronApplication.class, args);
+    }
+
+}
---
+++ src/test/java/norimaets/steamiron/SteamIronApplicationTests.java
@@ -0,0 +1,13 @@
+package norimaets.steamiron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamIronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
