2e7fcb7ac30dd3bd582d6fa31f48cedc8f864439
uhjin1130 <rladjwls1130@naver.com>
2026-07-16 17:50:01 +0900

    fix: users테이블 컬럼명 변경에 의한 엔티티 수정

--- module-domain-rdb/src/main/java/norimaets/moduledomainrdb/entity/User.java
+++ module-domain-rdb/src/main/java/norimaets/moduledomainrdb/entity/User.java
@@ -17,6 +17,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
     @Column(length = 100, nullable = false)
